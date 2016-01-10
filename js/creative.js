7428bd238fc4c036fa8f3c4593e19a126d3773c8
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2016-01-10 21:35:57 +0100

    First official deploy
    generated from commit 4358f264d6efae34d1bb4308680c18a9aee4f36e

--- js/creative.js
+++ js/creative.js
@@ -7,20 +7,26 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    var pageScrollFn = function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
-    });
+    };
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', pageScrollFn);
+    $('.section-a-page-scroll a').bind('click', pageScrollFn);
+
+
+
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
-    })
+    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -40,9 +46,39 @@
         offset: {
             top: 100
         }
-    })
+    });
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
+
+
+    // Pause button functionality for background video
+    var vid = document.getElementById("bgvid"),
+        pauseButton = document.getElementById("vidpause");
+
+    function vidFade() {
+        vid.classList.add("stopfade");
+    }
+
+    vid.addEventListener('ended', function() {
+        // only functional if "loop" is removed
+        vid.pause();
+        // to capture IE10
+        vidFade();
+        pauseButton.innerHTML = '<i class="fa fa-play fa-lg"></i>';
+    });
+
+    pauseButton.addEventListener("click", function() {
+        vid.classList.toggle("stopfade");
+        if (vid.paused) {
+            vid.play();
+            pauseButton.innerHTML = '<i class="fa fa-pause fa-lg"></i>';
+        } else {
+            vid.pause();
+            pauseButton.innerHTML = '<i class="fa fa-play fa-lg"></i>';
+        }
+    });
+
+
 
 })(jQuery); // End of use strict
